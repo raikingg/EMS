b6e3d7fac650bfcfeac74be7d911e21578c845f8
raishivan <shivanand.rai@impressico.com>
2021-11-20 12:13:03 +0530

    first

---
+++ springboot-backend/src/main/java/com/shivan/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.shivan.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shivan.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
